06d7678fbf18677816d61f40088e7d61aac0368d
Douglas <douglaszaltron@gmail.com>
2018-12-19 13:37:42 -0200

    Propriedades

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import { StyleSheet, View } from "react-native";
 
-type Props = {};
-export default class App extends Component<Props> {
+import Todo from "./components/Todo";
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to GoNative!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Todo title="Fazer café" />
       </View>
     );
   }
@@ -27,15 +20,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
   }
 });
