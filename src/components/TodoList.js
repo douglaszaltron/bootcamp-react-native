eb490322624313219f312343f3691129bc51211e
Douglas <douglaszaltron@gmail.com>
2018-12-19 16:17:27 -0200

    1 - Desafio

--- src/components/TodoList.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { View } from 'react-native';
-
-const TodoList = () => <View />;
-
-export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,88 @@
 import './config/ReactotronConfig';
 import './config/DevToolsConfig';
 
-import TodoList from '~/components/TodoList';
+import Post from '~/components/Post';
 
-import React from 'react';
+import React, { Component } from 'react';
 
 import {
-  StyleSheet, View, Text, Platform,
+  StyleSheet, View, Text, Platform, ScrollView,
 } from 'react-native';
 
-const bgColor = '#f5fcff';
+const white = '#FFF';
+const primary = '#EE7777';
+const title = '#333';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    flexWrap: 'wrap',
+    backgroundColor: primary,
+  },
+
+  header: {
+    backgroundColor: white,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: bgColor,
-  },
-  text: {
     ...Platform.select({
       ios: {
-        fontWeight: 'bold',
+        height: 76,
+        paddingTop: 20,
       },
       android: {
-        fontSize: 24,
+        height: 56,
+        paddingTop: 0,
       },
     }),
   },
+
+  headerTitle: {
+    color: title,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
 
-const App = () => (
-  <View style={styles.container}>
-    {Platform.OS === 'ios' ? <Text style={styles.text}>iOS</Text> : <Text>Android</Text>}
-    <TodoList />
-  </View>
-);
+export default class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'Aprendendo React Native',
+        author: 'Diego Fernandes',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 2,
+        title: 'Aprendendo React Native',
+        author: 'Diego Fernandes',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 3,
+        title: 'Aprendendo React Native',
+        author: 'Diego Fernandes',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+    ],
+  };
 
-export default App;
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>GoNative App</Text>
+        </View>
+        <ScrollView>
+          {posts.map(post => (
+            <Post key={post.id} data={post} />
+          ))}
+        </ScrollView>
+      </View>
+    );
+  }
+}
