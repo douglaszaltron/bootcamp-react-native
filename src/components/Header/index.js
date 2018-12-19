a63fffe1ff5e9834032ac6e6060f04b84f14a48b
Douglas <douglaszaltron@gmail.com>
2018-12-19 20:22:11 -0200

    Configurando Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import {
+  View, Text, StatusBar, TouchableOpacity,
+} from 'react-native';
+
+import styles from './styles';
+
+const Header = ({ title }) => (
+  <View style={styles.container}>
+    <StatusBar barStyle="dark-content" />
+    <View style={styles.left} />
+    <Text style={styles.title}>{title}</Text>
+    <TouchableOpacity onPress={() => {}} />
+  </View>
+);
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import Header from '~/components/Header';
 
 const Repositories = () => (
   <View>
-    <Text>Repositórios</Text>
+    <Header title="Repositórios" />
   </View>
 );
 
