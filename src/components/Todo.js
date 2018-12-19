273bd5905363ed598dce1c39bf7066151f3ffbe1
Douglas <douglaszaltron@gmail.com>
2018-12-19 14:02:05 -0200

    Stateful vs Stateless

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,24 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 import { View, Text, StyleSheet } from "react-native";
 
-export default class Todo extends Component {
-  static defaultProps = {
-    title: "Todo padrão"
-  };
+const Todo = ({ title }) => (
+  <View>
+    <Text>{title}</Text>
+  </View>
+);
 
-  static propTypes = {
-    title: PropTypes.string.isRequired
-  };
+Todo.defaultProps = {
+  title: "Todo padrão"
+};
 
-  render() {
-    return (
-      <View>
-        <Text>{this.props.title}</Text>
-      </View>
-    );
-  }
-}
+Todo.propTypes = {
+  title: PropTypes.string.isRequired
+};
 
 const styles = StyleSheet.create({});
+
+export default Todo;
