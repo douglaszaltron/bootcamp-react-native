98b5124fe1ac8700b876289358dda48b6a497dc9
Douglas <douglaszaltron@gmail.com>
2018-12-19 19:41:05 -0200

    Estilizando Welcome

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import {
+  View, Text, TextInput, TouchableOpacity, StatusBar,
+} from 'react-native';
 
 import styles from './styles';
 
 const Welcome = () => (
   <View style={styles.container}>
-    <Text>Testando</Text>
+    <StatusBar barStyle="light-content" />
+    <Text style={styles.title}>Bem-vindo</Text>
+    <Text style={styles.text}>
+      Para continuar precisamos que você informe seu usuário do GitHub
+    </Text>
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Digite seu usuário"
+        underlineColorAndroid="transparent"
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Prosseguir</Text>
+      </TouchableOpacity>
+    </View>
   </View>
 );
 
--- src/styles/metrics.js
+++ src/styles/metrics.js
@@ -3,8 +3,8 @@ import { Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
 
 export default {
-  baseMargin: '8',
-  basePadding: '16',
+  baseMargin: 8,
+  basePadding: 16,
   baseRadius: 3,
   screenWidth: width < height ? width : height,
   screenHeight: width < height ? height : width,
