f3501705fa6282dad3f581fdca9d5cabc88f15be
Douglas <douglaszaltron@gmail.com>
2018-12-19 13:57:02 -0200

    Estado & imutabilidade

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
 
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Button, Text } from "react-native";
 
 import Todo from "./components/Todo";
 
 export default class App extends Component {
+  state = {
+    usuario: "Douglas",
+    todos: [
+      {
+        id: 0,
+        text: "Fazer café"
+      },
+      {
+        id: 1,
+        text: "Estudar o GoNative"
+      }
+    ]
+  };
+
+  addTodo = () => {
+    this.setState({
+      todos: [...this.state.todos, { id: Math.random(), text: "Novo todo" }]
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Todo title="Fazer café" />
-        <Todo title={5} />
+        <Text>{this.state.usuario}</Text>
+        {this.state.todos.map(todo => (
+          <Todo key={todo.id} title={todo.text} />
+        ))}
+        <Button title="Adicionar todo" onPress={this.addTodo} />
       </View>
     );
   }
