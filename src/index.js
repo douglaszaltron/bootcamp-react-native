17655e7663021a8b315e292fa4450f3e9acdcb06
Douglas <douglaszaltron@gmail.com>
2018-12-19 16:01:07 -0200

    Manipulando Android vs iOS

--- src/index.js
+++ src/index.js
@@ -5,23 +5,36 @@ import TodoList from '~/components/TodoList';
 
 import React from 'react';
 
-import { StyleSheet, View } from 'react-native';
+import {
+  StyleSheet, View, Text, Platform,
+} from 'react-native';
 
 const bgColor = '#f5fcff';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
     flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    alignContent: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: bgColor,
+  },
+  text: {
+    ...Platform.select({
+      ios: {
+        fontWeight: 'bold',
+      },
+      android: {
+        fontSize: 24,
+      },
+    }),
   },
 });
 
 const App = () => (
   <View style={styles.container}>
+    {Platform.OS === 'ios' ? <Text style={styles.text}>iOS</Text> : <Text>Android</Text>}
     <TodoList />
   </View>
 );
