61f40aac750a3d6fff1132a7d9593cd56dad0e1b
Douglas <douglaszaltron@gmail.com>
2018-12-20 17:43:21 -0200

    Organizações

--- src/pages/Organizations/OrganizationItem/styles.js
+++ src/pages/Organizations/OrganizationItem/styles.js
@@ -3,39 +3,27 @@ import { colors, metrics } from '~/styles';
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: colors.white,
     borderRadius: metrics.baseRadius,
     padding: metrics.basePadding,
-    marginHorizontal: metrics.baseMargin * 2,
     marginTop: metrics.baseMargin,
+    alignItems: 'center',
+    maxWidth: (metrics.screenWidth - 40) / 2,
     borderBottomColor: colors.lightTransparent,
     borderBottomWidth: 1,
+  },
+
+  avatar: {
+    width: 50,
+    height: 50,
   },
 
   title: {
     fontSize: 14,
     fontWeight: 'bold',
-  },
-
-  infoContainer: {
-    flexDirection: 'row',
+    color: colors.darker,
     marginTop: metrics.baseMargin,
-  },
-
-  info: {
-    flexDirection: 'row',
-    marginRight: metrics.baseMargin,
-    alignItems: 'center',
-  },
-
-  infoIcon: {
-    color: colors.dark,
-  },
-
-  infoText: {
-    color: colors.dark,
-    fontSize: 12,
-    marginLeft: metrics.baseMargin / 2,
   },
 });
 
--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import api from '~/services/api';
 
-import { View } from 'react-native';
+import {
+  View, AsyncStorage, ActivityIndicator, FlatList,
+} from 'react-native';
 
 import Icon from 'react-native-ionicons';
 
 import Header from '~/components/Header';
-
-const Organizations = () => (
-  <View>
-    <Header title="Organizações" />
-  </View>
-);
+import OrganizationItem from './OrganizationItem';
+import styles from './styles';
 
 const TabIcon = ({ tintColor }) => <Icon name="business" size={26} color={tintColor} />;
 
@@ -19,8 +18,55 @@ TabIcon.propTypes = {
   tintColor: PropTypes.string.isRequired,
 };
 
-Organizations.navigationOptions = {
-  tabBarIcon: TabIcon,
-};
+export default class Organizations extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon,
+  };
 
-export default Organizations;
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false,
+  };
+
+  async componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    this.setState({ refreshing: true });
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const { data } = await api.get(`/users/${username}/orgs`);
+
+    this.setState({ data, loading: false, refreshing: false });
+  };
+
+  renderListItem = ({ item }) => <OrganizationItem organization={item} />;
+
+  renderList = () => {
+    const { data, refreshing } = this.state;
+
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+        numColumns={2}
+        columnWrapperStyle={styles.columnWrapper}
+        onRefresh={this.loadOrganizations}
+        refreshing={refreshing}
+      />
+    );
+  };
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Header title="Organizações" />
+        {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
+      </View>
+    );
+  }
+}
