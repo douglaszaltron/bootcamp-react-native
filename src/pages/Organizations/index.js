d2c2aa8994e05fc36b64018be9367ed5d2053a9e
Douglas <douglaszaltron@gmail.com>
2018-12-20 16:10:29 -0200

    Estilizando abas

--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { View } from 'react-native';
 
-const Organizations = () => <View />;
+import Icon from 'react-native-ionicons';
+
+import Header from '~/components/Header';
+
+const Organizations = () => (
+  <View>
+    <Header title="Organizações" />
+  </View>
+);
+
+const TabIcon = ({ tintColor }) => <Icon name="business" size={26} color={tintColor} />;
+
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Organizations.navigationOptions = {
+  tabBarIcon: TabIcon,
+};
 
 export default Organizations;
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { View } from 'react-native';
+
+import Icon from 'react-native-ionicons';
 
 import Header from '~/components/Header';
 
@@ -9,5 +12,15 @@ const Repositories = () => (
     <Header title="Repositórios" />
   </View>
 );
+
+const TabIcon = ({ tintColor }) => <Icon name="list" size={26} color={tintColor} />;
+
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Repositories.navigationOptions = {
+  tabBarIcon: TabIcon,
+};
 
 export default Repositories;
