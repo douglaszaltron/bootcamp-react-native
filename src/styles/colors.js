d6e3741a67c6c888f920436e1e9862572d1cf124
Douglas <douglaszaltron@gmail.com>
2019-01-22 15:28:32 -0200

    Estilos globais

---
+++ src/styles/colors.js
@@ -0,0 +1,18 @@
+export default {
+  white: '#fff',
+  lighter: '#eee',
+  light: '#ddd',
+  regular: '#999',
+  dark: '#666',
+  darker: '#333',
+  black: '#000',
+
+  primary: '#6550b7',
+  secondary: '#6adc00',
+  success: '#9dca83',
+  danger: '#e37a7a',
+
+  transparent: 'transparent',
+  darkTransparent: 'rgba(0,0,0,0.6)',
+  lightTransparent: 'rgba(255,255,255,0.3)',
+};
